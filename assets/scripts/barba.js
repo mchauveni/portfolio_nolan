d7239fa2bb3d3b5f2d8f3a48ddd4fe235db9a18e
Nol-lu <Nolan.luzet@gmail.com>
2024-02-01 13:19:12 +0100

    Change

---
+++ assets/scripts/barba.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", function(){
+    barba.init({
+        transitions: [{
+          name: 'opacity-transition',
+          leave(data) {
+            return gsap.to(data.current.container, {
+              opacity: 0
+            });
+          },
+          enter(data) {
+            return gsap.from(data.next.container, {
+              opacity: 0
+            });
+          }
+        }]
+      });
+})
